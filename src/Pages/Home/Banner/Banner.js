75df4b56ac6ccaa99d4de5be117332424a996b01
MdShohan81 <mdshohan6563@gmail.com>
2022-09-01 12:17:13 +0600

    added padding

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -17,7 +17,7 @@ const Banner = () => {
       <div className='banner'>
         <div className="banner-overlay"></div>
         <video src={bannerbg} autoPlay loop muted/>
-      <div className='banner-content'>
+      <div className='banner-content px-2'>
       <h1 className='mb-5 text-primary  text-4xl lg:text-5xl font-bold'>Find Whatever Your Want?</h1>
       <p className='text-center text-xl lg:text-2xl md:text-2xl text-white'>A shadowy flight into the dangerous world of a man who does not exist.</p>
       <p className='text-center mb-10 text-xl lg:text-2xl md:text-2xl text-white'>Flying away on a wing and a prayer. Who could it be.</p>
